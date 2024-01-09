2f1318d036df963b2af6bac9f45e9b2fc51ef16a
Wesley Sousa <wlywesley123@gmail.com>
2024-01-09 18:56:07 -0300

    Working with four verbs

---
+++ RestWithASPNET/RestWithASPNET/Controllers/PersonController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNET.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            return BadRequest("Invalid Input");
+        }
+    }
+}
